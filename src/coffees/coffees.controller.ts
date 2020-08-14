cdc45f87de223a85d66089d9941df4e885421e9c
Pedro Javier <pjpc@ic.ufal.br>
2020-08-13 22:36:33 -0300

    Add patch and delete methods

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Patch, Delete } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -16,5 +16,15 @@ export class CoffeesController {
     create(@Body() body) {
         return body;
         // return `this action creates a coffee`;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body): string {
+        return `This action updates #${id} coffee`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string): string {
+        return `This action removes #${id} coffee`;
     }
 }
