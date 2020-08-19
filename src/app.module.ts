ce380f9ce9bef0c1843a262fcd3f3e3e62ba491c
Pedro Javier <pjpc@ic.ufal.br>
2020-08-19 12:04:22 -0300

    Create dynamic module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import {TypeOrmModule} from '@nestjs/typeorm'
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [CoffeesModule, TypeOrmModule.forRoot({
@@ -17,7 +18,7 @@ import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
     // development only
     synchronize: true
-  }), CoffeeRatingModule],
+  }), CoffeeRatingModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CoffeeRatingService } from './coffee-rating.service';
 import { CoffeesModule } from 'src/coffees/coffees.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    DatabaseModule.register({ // 👈 passing in dynamic values
+      type: 'postgres',
+      host: 'localhost',
+      password: 'password',
+    }),
+    CoffeesModule
+  ],
   providers: [CoffeeRatingService]
 })
 export class CoffeeRatingModule {}
