309af2e48a87c652ab79263c36340d923875c924
Pedro Javier <pjpc@ic.ufal.br>
2020-08-19 11:25:28 -0300

    Class Providers basead on the configService

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -8,10 +8,24 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
 
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
-    providers: [CoffeesService, { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }],
+    providers: [
+        CoffeesService, 
+        { 
+            provide: ConfigService,
+            useClass: process.env.NODE_ENV === 'development' ? DevelopmentConfigService : ProductionConfigService,
+        }, 
+        { 
+            provide: COFFEE_BRANDS, 
+            useValue: ['buddy brew', 'nescafe'] 
+        }
+    ],
     exports: [CoffeesService]
 })
 export class CoffeesModule {}
